2502332c6acbe9501dd818c3eb992dd0be114b9a
ShyamKumar11 <shyamkumarlohat@gmail.com>
2024-04-30 16:26:26 -0600

    CSS JAVA updates

---
+++ scripts/getdates.js
@@ -0,0 +1,9 @@
+// Sets the current year
+let year = new Date().getFullYear();
+let currentYearspan = document.querySelector("#currentYear");
+currentYearspan.innerHTML = `${year}`;
+
+//sets the last modified year
+// let lastModifiedParagraph = document.querySelector("#lastModified")
+let lastModifiedParagraph = document.getElementById("lastModified");
+lastModifiedParagraph.innerHTML = `last Modified: ${document.lastModified}`;
