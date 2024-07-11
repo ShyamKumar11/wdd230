3fafa46eb161ca2930fd60d9d5b645bfffbcfe5c
ShyamKumar11 <shyamkumarlohat@gmail.com>
2024-07-11 14:05:46 -0600

    home page touchups

--- scripts/weather-homep.js
+++ scripts/weather-homep.js
@@ -7,7 +7,7 @@ const weatherIcon = document.querySelector("#weather-icon");
 
 function displayResults(data) {
   currentTemp.innerHTML = `${data.main.temp}&deg;F`;
-  // captionDesc.innerText = data.weather[0].main;
+  //captionDesc.innerText = data.weather[0].main;
   weatherIcon.setAttribute(
     "src",
     "https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png"
