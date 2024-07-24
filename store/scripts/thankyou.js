f7e71323e74d7d3490a59db89d6f83ed9ba1dae2
ShyamKumar11 <shyamkumarlohat@gmail.com>
2024-07-24 14:02:36 -0600

    All over updates

--- store/scripts/thankyou.js
+++ store/scripts/thankyou.js
@@ -2,9 +2,10 @@ window.onload = () => {
   const urlParams = new URLSearchParams(window.location.search);
   document.getElementById("yourname").textContent =
     urlParams.get("first_name") + " " + urlParams.get("last_name");
-  document.getElementById("yourtitle").textContent = urlParams.get("title");
   document.getElementById("yourlocation").textContent =
     urlParams.get("location");
   document.getElementById("youremail").textContent = urlParams.get("email");
-  // Add more fields as needed
+  document.getElementById("yourexp").textContent = urlParams.get("expedition");
+  document.getElementById("yourlocation").textContent =
+    urlParams.get("location");
 };
