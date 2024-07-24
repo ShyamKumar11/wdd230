2c7ecc3cf9d1c21a80aa8b8a3b1ac7c653d14b1a
ShyamKumar11 <shyamkumarlohat@gmail.com>
2024-07-24 17:51:22 -0600

    submission page work

--- store/scripts/thankyou.js
+++ store/scripts/thankyou.js
@@ -9,3 +9,10 @@ window.onload = () => {
   document.getElementById("yourlocation").textContent =
     urlParams.get("location");
 };
+
+// Calculate estimated shipping date
+const shippingDate = new Date();
+shippingDate.setDate(shippingDate.getDate() + 5);
+const options = { year: "numeric", month: "long", day: "numeric" };
+document.getElementById("shipping-date").textContent =
+  shippingDate.toLocaleDateString(undefined, options);
