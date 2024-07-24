f1642ea6881db09a5c122b26be77e760bccd4f0a
ShyamKumar11 <shyamkumarlohat@gmail.com>
2024-07-24 01:04:38 -0600

    footer updates

---
+++ store/scripts/footer.js
@@ -0,0 +1,7 @@
+// footer.js
+
+// Set current year
+document.getElementById("currentYear").textContent = new Date().getFullYear();
+
+// Set last modified date
+document.getElementById("lastModified").textContent = document.lastModified;
